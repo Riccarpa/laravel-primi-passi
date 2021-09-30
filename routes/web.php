0223f5e4c975a1e93f852d0d6bc615d2024a1fe7
Riccarpa <riccardopagni99@gmail.com>
2021-09-30 15:21:13 +0200

    route data on home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data=['home','news','info','faqs'];
+    
+
+    return view('home', ['data'=> $data]);
 });
